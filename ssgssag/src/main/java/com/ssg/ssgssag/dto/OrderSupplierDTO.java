eaa06ae77a481516f348d44b2272613bff2edad7
zzzjinwook <zzzjinwook@naver.com>
2024-03-20 15:48:38 +0900

    feat : 매입거래처 DTO 생성

---
+++ ssgssag/src/main/java/com/ssg/ssgssag/dto/OrderSupplierDTO.java
@@ -0,0 +1,15 @@
+package com.ssg.ssgssag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderSupplierDTO {
+
+    private String vIncomingProductSupplierNm;
+}
