ff6681160aeb32b0054c0fda4f9492c6de1aaaf8
zzzjinwook <zzzjinwook@naver.com>
2024-03-22 22:01:15 +0900

    feat : 좌표 여러개 찍기

--- ssgssag/src/main/resources/static/js/WarehouseMap.js
+++ ssgssag/src/main/resources/static/js/WarehouseMap.js
@@ -1,38 +1,34 @@
-
-
 let mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
       center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
       level: 3 // 지도의 확대 레벨
     };
 
-// 지도를 생성합니다
-let map = new kakao.maps.Map(mapContainer, mapOption);
+let map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+let geocoder = new kakao.maps.services.Geocoder(); // 주소-좌표 변환 객체를 생성합니다
 
-// 주소-좌표 변환 객체를 생성합니다
-let geocoder = new kakao.maps.services.Geocoder();
+window.onload = function () {
+  // 테이블에서 창고소재지 정보를 추출합니다
+  let addresses = Array.from(
+      document.querySelectorAll('.warehouseTable tbody tr')).map(
+      function (row) {
+        return row.cells[3].innerText; // 창고소재지가 4번째 열에 위치
+      });
 
-// 주소로 좌표를 검색합니다
-geocoder.addressSearch('제주특별자치도 제주시 첨단로 242', function (result, status) {
+  // 추출한 주소로 마커를 생성합니다
+  addresses.forEach(function (address) {
+    geocoder.addressSearch(address, function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-  // 정상적으로 검색이 완료됐으면
-  if (status === kakao.maps.services.Status.OK) {
+        let marker = new kakao.maps.Marker({
+          map: map,
+          position: coords
+        });
 
-    let coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-    // 결과값으로 받은 위치를 마커로 표시합니다
-    let marker = new kakao.maps.Marker({
-      map: map,
-      position: coords
+        // 지도의 중심을 마지막 마커의 위치로 이동시킵니다 (선택적)
+        map.setCenter(coords);
+      }
     });
-
-    // 인포윈도우로 장소에 대한 설명을 표시합니다
-    let infowindow = new kakao.maps.InfoWindow({
-      content: '<div style="width:150px;text-align:center;padding:6px 0;">우리회사</div>'
-    });
-    infowindow.open(map, marker);
-
-    // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-    map.setCenter(coords);
-  }
-});
+  });
+};
