dbc48cd833d1c69b50ae26a617028bd363361da5
zzzjinwook <zzzjinwook@naver.com>
2024-03-22 11:54:48 +0900

    feat : 대시보드 status 조회 DTO, mapper 생성

---
+++ ssgssag/src/main/java/com/ssg/ssgssag/dto/StatusCountDTO.java
@@ -0,0 +1,16 @@
+package com.ssg.ssgssag.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusCountDTO {
+
+    private String section;
+    private int count;
+}
