6e446e81de5c85ba5173da5413d5cf73d60ddfd2
SonHyeJi <kuy06046@naver.com>
2024-03-23 14:24:03 +0900

    style : 불필요한 주석 제거

--- ssgssag/src/main/resources/static/js/WarehouseMap.js
+++ ssgssag/src/main/resources/static/js/WarehouseMap.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // 창고 리스트 페이지 지도 초기화
+
   initializeListMap();
 
-  // 모달에서 창고 상세보기를 위한 지도 초기화 이벤트 리스너 추가
   document.querySelectorAll('.btn-warehouse-modal').forEach(button => {
     button.addEventListener('click', function() {
       const address = this.getAttribute('data-warehouse-loc'); // 'this'는 클릭된 버튼을 가리킵니다.
@@ -32,7 +31,6 @@ function initializeListMap() {
           position: coords
         });
         map.setCenter(coords);
-        // setTimeout(function() { modalMap.relayout(); }, 200);
       }
     });
   });
@@ -58,7 +56,7 @@ function initializeModalMap(address) {
         position: coords
       });
 
-      // parseFloat()를 사용해 명시적으로 숫자형으로 변환
+
       let x = parseFloat(result[0].x);
       let y = parseFloat(result[0].y);
 
@@ -67,15 +65,15 @@ function initializeModalMap(address) {
 
       // 변환된 숫자에 0.001을 더함
       let adjustedX = x - 0.005;
-      let adjustedY = y + 0.002; // 예시로, y 값도 조정해봅니다.
+      let adjustedY = y + 0.002;
 
-      console.log(adjustedX); // 변환 후 결과를 확인하기 위한 로그
-      console.log(adjustedY); // 변환 후 결과를 확인하기 위한 로그
+      console.log(adjustedX);
+      console.log(adjustedY);
 
-      let adjustedCenter = new kakao.maps.LatLng(adjustedY, adjustedX); // 경도 조정 값
+      let adjustedCenter = new kakao.maps.LatLng(adjustedY, adjustedX);
 
       modalMap.setCenter(adjustedCenter);
-      setTimeout(function() { modalMap.relayout(); }, 200);  // 지도 레이아웃을 재설정
+      setTimeout(function() { modalMap.relayout(); }, 200);
 
     } else {
       console.error('Failed to search address:', address);
