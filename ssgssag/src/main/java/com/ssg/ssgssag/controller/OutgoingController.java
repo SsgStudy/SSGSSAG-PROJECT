f72dfb4c6ec000aaf63ab6396768e5c95ea6395c
SonHyeJi <kuy06046@naver.com>
2024-03-23 19:26:59 +0900

    chore : 출고관리 전체 목록 스웨거 적용

--- ssgssag/src/main/java/com/ssg/ssgssag/controller/OutgoingController.java
+++ ssgssag/src/main/java/com/ssg/ssgssag/controller/OutgoingController.java
@@ -3,6 +3,7 @@ package com.ssg.ssgssag.controller;
 import com.ssg.ssgssag.domain.OutgoingProductVO;
 import com.ssg.ssgssag.dto.IncomingDTO;
 import com.ssg.ssgssag.service.OutgoingService;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,6 +21,7 @@ public class OutgoingController {
   private final OutgoingService outgoingService;
 
   @GetMapping()
+  @Operation(summary = "출고 목록 조회", description = "전체 출고 목록을 조회합니다.")
   public String showOutgoingListPage(Model model) {
     log.info("outgoing controller test");
 
