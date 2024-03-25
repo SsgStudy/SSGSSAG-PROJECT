bc6f8b89ec7b7f6343822b71aa6dac4f61323d65
zzzjinwook <zzzjinwook@naver.com>
2024-03-25 20:10:58 +0900

    fix : gpt 모델 설정

--- ssgssag/src/main/java/com/ssg/ssgssag/config/ChatGptConfig.java
+++ ssgssag/src/main/java/com/ssg/ssgssag/config/ChatGptConfig.java
@@ -4,7 +4,7 @@ public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
     public static final String API_KEY = "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";
-    public static final String MODEL = "gpt-3.5-turbo";
+    public static final String MODEL = "gpt-3.5-turbo-0125";
     public static final Integer MAX_TOKEN = 2000;
     public static final Double TEMPERATURE = 0.0;
     public static final Double TOP_P = 1.0;
