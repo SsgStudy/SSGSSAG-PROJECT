f56be33e11df0c02fbc3b63730bab5a4ffd7785c
zzzjinwook <zzzjinwook@naver.com>
2024-03-26 02:21:41 +0900

    feat : GPT 귀여움 한스푼 추가

--- ssgssag/src/main/resources/static/js/Main.js
+++ ssgssag/src/main/resources/static/js/Main.js
@@ -65,28 +65,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // 문자열을 JSON 객체로 변환합니다.
   const data = {
-    question: "카테고리 별 판매 건수를 제공할거야. 자세한 분석 포인트와 해당 근거, 판매 건수를 늘리거나 재고 관련 최대한 자세한 제안을 Json 형태로 전달해줘. 'analysis'과 'suggestion' 단 두 개의 키 값만 사용하도록 해줘. 단순한 키-밸류 구조를 유지해줘. "+ JSON.stringify(bestCategoryDTOList)
+    question: "인기 카테고리 별 판매 건수를 제공할거야. 자세한 분석 포인트와 해당 근거, 판매 건수를 늘리거나 재고 관련 최대한 자세한 제안을 Json 형태로 전달해줘. 'analysis'과 'suggestion' 단 두 개의 키 값만 사용하도록 해줘. 단순한 키-밸류 구조를 유지해줘. "+ JSON.stringify(bestCategoryDTOList)
+    // question: "비인기 카테고리 별 판매 건수를 제공할거야. 자세한 분석 포인트와 해당 근거, 판매 건수를 늘리거나 재고 관련 최대한 자세한 제안을 Json 형태로 전달해줘. 'analysis'과 'suggestion' 단 두 개의 키 값만 사용하도록 해줘. 단순한 키-밸류 구조를 유지해줘. "+ JSON.stringify(bestCategoryDTOList)
   };
   console.log(bestCategoryDTOList);
   console.log(data);
-  document.getElementById('best-category-analysis-text').innerText = "GPT 결과 분석 중";
+  document.getElementById('best-category-analysis-text').innerText = "AI가 인기 카테고리 요인을 열심히 분석하고 있어요~~   (｡ì _ í｡)";
+  document.getElementById('best-category-suggestion-text').innerText = "판매율을 높이기 위한 방법을 생각 중이에요!!  (￣◇￣;)";
 
-  // fetch('/question', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify(data) // 수정된 부분
-  // })
-  // .then(response => response.json())
-  // .then(data => {
-  //   console.log('Success:', data);
-  //   document.getElementById('best-category-analysis-text').innerText = data.analysis;
-  //   document.getElementById('best-category-suggestion-text').innerText = data.suggestion;
-  // })
-  // .catch((error) => {
-  //   console.error('Error:', error);
-  // });
   fetch('/question', {
     method: 'POST',
     headers: {
