d8f211b3851e95b5b48f733fbddfa6cb75c3df43
장서윤 <seoy316@naver.com>
2024-03-28 15:52:03 +0900

    feat: 대시보드 프로필 이미지 출력

--- ssgssag/src/main/resources/static/js/dashboard/profile.js
+++ ssgssag/src/main/resources/static/js/dashboard/profile.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    console.log('실행?');
     $.ajax({
         url: '/member/profile-img',
         method: 'GET',
@@ -7,12 +6,11 @@ $(document).ready(function () {
             responseType: 'blob'
         },
         success: function (data) {
-            if (data) {
+            console.log(data);
+            if (data.size > 0) {
                 let urlCreator = window.URL || window.webkitURL;
                 let imageUrl = urlCreator.createObjectURL(data);
                 $('.user-profile-img').attr('src', imageUrl);
-            } else {
-                $('.user-profile-img').attr('src', '/images/profile/default-img.png');
             }
         },
         error: function(xhr, status, error) {
